e8d210200a33190cb596863d8624328a4ab6c69a
tbmpang <tbmpang@hotmail.com>
2023-10-11 20:23:12 +0200

    Version 1.1

--- switch-statements.js
+++ switch-statements.js
@@ -2,7 +2,7 @@
 // Opdracht 1: schrijf een switch statement die op basis van de volgende variabele een bijbehorende foutmelding in de console logt.
 // ==========================================
 
-const errorCode = 0;
+const errorCode = 4;
 
 // Je mag ervanuit gaan dat deze variabele altijd een waarde tussen de 0 en de 4 bevat. Deze codes hebben de volgende betekenissen (foutmeldingen):
 // 0: "Geen watertoevoer"
@@ -10,6 +10,23 @@ const errorCode = 0;
 // 2: "Koffiebonen op"
 // 3: "Afvalbak vol"
 // 4: "Geen druk"
+switch (errorCode) {
+    case 0 :
+        console.log("Geen watertoevoer");
+        break;
+    case 1 :
+        console.log("Temperatuur te laag");
+        break;
+    case 2 :
+        console.log("Koffiebonen op");
+        break;
+    case 3 :
+        console.log("Afvalbak vol");
+        break;
+    case 4 :
+        console.log("Geen druk");
+        break;
+}
 
 // Verwachtte uitkomsten:
 // const errorCode = 0 geeft "Geen watertoevoer" in de terminal
@@ -31,6 +48,22 @@ const monitorColor = "red";
 // "red": "Kritieke hartslag, actie vereist!"
 // Bij alle andere kleuren is de status van de hartslag onbekend.
 
+switch (monitorColor) {
+    case "green" :
+        console.log("Hartslag stabiel");
+        break;
+    case "yellow" :
+        console.log("Hartslag verhoogd");
+        break;
+    case "orange" :
+    case "red":
+        console.log("Kritieke hartslag, actie vereist!");
+        break;
+
+    default :
+        console.log("Status van de hartslag onbekend")
+
+}
 // Verwachtte uitkomsten:
 // const monitorColor = "red" geeft "Kritieke hartslag, actie vereist!" in de terminal
 // const monitorColor = "green" geeft "Hartslag stabiel" in de terminal
